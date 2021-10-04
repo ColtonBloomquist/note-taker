8b4a4d29890b6484489e72f50922b8e76b6a1930
colton bloomquist <coltonbloomquist@yahoo.com>
2021-10-04 15:06:08 -0600

    restructured files for simplicity. Will refactor later

--- server.js
+++ server.js
@@ -1,13 +1,46 @@
+const { json } = require("express");
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3001;
 const fs = require("fs");
 const path = require("path");
+const allNotes = require("./db/db.json");
 
+//middleware to process json
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
+// routes for html
+
+//routes for index.html
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+// route for notes.html
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
 app.listen(PORT, function () {
   console.log(`Server is listening on PORT: ${PORT}`);
 });
+
+function createNewNote(body, notesArray) {
+  const newNote = body;
+
+  notesArray.push(newNote);
+  fs.writeFileSync(
+    path.join(__dirname, "./db/db.json"),
+    JSON.stringify({ notes: notesArray }, null, 2)
+  );
+  return newNote;
+}
+
+//routes for api
+app.post("/notes", (req, res) => {});
