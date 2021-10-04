80c63f4801ba079354ac95100a17cb312244986a
colton bloomquist <coltonbloomquist@yahoo.com>
2021-10-04 13:59:25 -0600

    asdf

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3001;
+const fs = require("fs");
+const path = require("path");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
